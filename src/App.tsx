d4396c6a85ff331d3845ebcc38429d94bdf017c2
polamjag <s@polamjag.info>
2021-02-19 20:56:29 +0900

    gomi

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import firebase from "firebase";
+import "./App.css";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB2jJu7vUNztDiVbaDTvjKAFjRZQzDCDfo",
+  authDomain: "gjxgjx-35640.firebaseapp.com",
+  databaseURL: "https://gjxgjx-35640-default-rtdb.firebaseio.com",
+  projectId: "gjxgjx-35640",
+  storageBucket: "gjxgjx-35640.appspot.com",
+  messagingSenderId: "192829888990",
+  appId: "1:192829888990:web:75b160ac76dae99555654f",
+};
+
+const fb = firebase.initializeApp(firebaseConfig);
+const database = fb.database();
 
 function App() {
+  const [gomi, setGomi] = React.useState<string>("");
+  const handleChange = (ev: React.ChangeEvent<HTMLInputElement>) => {
+    setGomi(ev.target.value);
+
+    database.ref("gomi/").set({
+      contents: ev.target.value,
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input value={gomi} onChange={handleChange} autoCorrect='off' />
     </div>
   );
 }
