113c31ef1fa0b7deed313b07318ae0ee151d7053
polamjag <s@polamjag.info>
2021-02-20 02:31:02 +0900

    + manualswitcher

--- src/gjx/components/Switcher.tsx
+++ src/gjx/components/Switcher.tsx
@@ -18,7 +18,7 @@ export const Switcher: React.FC<{}> = () => {
     case "intervalSwitching":
       return <IntervalSwitcher />;
     case "manualSwitching":
-      return <></>;
+      return <ManualSwitcher />;
   }
 };
 
@@ -36,7 +36,7 @@ const IntervalSwitcher: React.FC<{}> = () => {
       const newState: SwitchingStrategies = {
         ...oldState,
         intervalSwitching: {
-          name: 'intervalSwitching',
+          name: "intervalSwitching",
           state: {
             intervalMs: Number(value),
           },
@@ -74,6 +74,50 @@ const IntervalSwitcher: React.FC<{}> = () => {
         onChange={onChange}
       />
       {switchingStrategy.intervalSwitching.state.intervalMs}ms
+    </div>
+  );
+};
+
+const ManualSwitcher: React.FC<{}> = () => {
+  const [switchingStrategy, setSwitchingStrategy] = useRecoilState(
+    switchingStrategyState
+  );
+  const images = useRecoilValue(imagesState);
+  const [, setSelectedImage] = useRecoilState(selectedImageState);
+
+  const onChange = ({
+    target: { value },
+  }: React.ChangeEvent<HTMLInputElement>) => {
+    const val = Number(value);
+    setSwitchingStrategy((oldState: SwitchingStrategies) => {
+      const newState: SwitchingStrategies = {
+        ...oldState,
+        manualSwitching: {
+          name: "manualSwitching",
+          state: {
+            index: val,
+          },
+        },
+      };
+      return newState;
+    });
+
+    setSelectedImage(
+      Object.values(images)[Math.floor(val * Object.values(images).length)]
+    );
+  };
+
+  return (
+    <div className="switcher switcher__interval">
+      <div>Manual Switcher</div>
+      <input
+        type="range"
+        value={switchingStrategy.manualSwitching.state.index}
+        min={0}
+        max={0.99}
+        step={0.01}
+        onChange={onChange}
+      />
     </div>
   );
 };
