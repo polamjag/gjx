d2ada57ae42a585b1e7a8157f1f0ac40282892ee
polamjag <s@polamjag.info>
2021-02-20 17:27:31 +0900

    deletable

--- src/gjx/components/controllers/ImagesList.tsx
+++ src/gjx/components/controllers/ImagesList.tsx
@@ -2,16 +2,33 @@ import React from "react";
 import { useRecoilState } from "recoil";
 
 import { imagesState } from "../../atoms";
+import { ThumbnailWithAction } from "../molecures/ThumbnailWithAction";
 
 export const ImagesList: React.FC<{}> = () => {
-  const [images] = useRecoilState(imagesState);
+  const [images, setImagesState] = useRecoilState(imagesState);
+
+  const deleteImageByKey = (key: string): void => {
+    const newImages = { ...images };
+    delete newImages[key];
+    setImagesState(
+      Object.fromEntries(
+        Object.values(newImages).map((url, index) => [index.toString(), url])
+      )
+    );
+  };
 
   return (
     <>
       <ul className="images-list">
         {Object.entries(images).map(([id, image]) => (
           <li key={id}>
-            <img src={image} alt="" />
+            <ThumbnailWithAction
+              thumbnailUrl={image}
+              buttonLabel="x"
+              onClickButton={() => {
+                deleteImageByKey(id);
+              }}
+            />
           </li>
         ))}
       </ul>
