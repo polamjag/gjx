f931a4cd87d6a4564e7bd0912b7f2c42df842e4d
polamjag <s@polamjag.info>
2021-02-23 14:10:50 +0900

    import only some of firebase modules

--- src/gjx/components/WithFirebase.tsx
+++ src/gjx/components/WithFirebase.tsx
@@ -1,4 +1,6 @@
-import firebase from "firebase";
+import firebase from "firebase/app";
+import "firebase/database";
+
 import React, { createContext, useEffect, useState } from "react";
 
 export const WithFirebase: React.FC<{ firebaseConfig: Object }> = ({
